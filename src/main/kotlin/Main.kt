83cd45e3c3ee21aa1bce93dea0103682cd095f73
Hudzone <fyodor.blyudov@gmail.com>
2024-02-03 19:09:47 +0300

    Kotlin. Lessons 1-4. Refactored for using when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,18 +17,16 @@ fun main() {
 
 private fun readBountyBoard() {
     println("$HERO_NAME approaches the bounty board. New task: ")
-    println(obtainQuest(playerLevel, false, true, "paladin"))
+    println(obtainQuest(playerLevel))
 }
 
 private fun obtainQuest(
     // заголовок функции
     playerLevel: Int,
-    hasAngeredBarbarians: Boolean,
-    hasBeFriendedBarbarians: Boolean,
-    playerClass: String
-): String {
-    // тело функции
-    val quest: String = when (playerLevel) {
+    playerClass: String = "paladin",
+    hasBeFriendedBarbarians: Boolean = true,
+    hasAngeredBarbarians: Boolean = false
+): String = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things"
         in 2..5 -> {
             // Проверить возможность дипломатического решения
@@ -46,5 +44,3 @@ private fun obtainQuest(
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There is no quest right now"
     }
-    return quest
-}
